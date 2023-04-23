f4a61e55f9946870ed4727bca601a2abed423f63
vanya992 <vanjacrv92@gmail.com>
2023-04-23 13:58:13 +0200

    now?

--- js/components/featured.js
+++ js/components/featured.js
@@ -8,10 +8,10 @@ fetch('http://simplygreatno.local/wp-json/wc/store/products?featured=true')
     displayFeatured.classList.add("featured_section");
     
     featuredProducts.forEach(product => {
-        displayFeatured.innerHTML += `<div class="featured">
+        displayFeatured.innerHTML += `<a href="details.html?id=${product.id}"><div class="featured">
       <h2>${product.name}</h2>
         <img src="${product.images[0].src}">
-      <p>Price: ${product.price_html}</p></div>`;
+      <p>Price: ${product.price_html}</p></div></a>`;
     });
 
     const featuredSection = document.querySelector(".featured_section");
